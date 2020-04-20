a4782b38884d560c16828954c46eb5134cf0987d
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-04-20 15:08:05 -0700

    Added quiz files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+      AppComponent,
+      QuestionFormComponent
+    ],
+    imports: [
+      HttpClientModule,
+      BrowserModule,
+      ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
