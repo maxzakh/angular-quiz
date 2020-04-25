b275a930d3bea62ef7c0586cdf9f11728f501250
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-04-25 00:26:11 -0700

    finished skeleton for quiz app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { ResultsComponent } from './results/results.component';
 
-import { AppComponent } from './app.component';
-import { QuestionFormComponent } from './question-form/question-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
-    declarations: [
-      AppComponent,
-      QuestionFormComponent
-    ],
-    imports: [
-      HttpClientModule,
-      BrowserModule,
-      ReactiveFormsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, QuestionFormComponent, QuestionsComponent, ResultsComponent],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    BrowserModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
