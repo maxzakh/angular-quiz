1ba4b3aa3c6c5e17d9f1099d90e6dc5eee081d30
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-05-15 12:18:31 -0700

    Added opentdb service

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -2,7 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Quiz, Question } from './quiz.model';
+import { Quiz, Question, Choice } from './quiz.model';
+
+type TriviaCategories = {
+  trivia_categories: {
+    id: number;
+    name: string;
+  }[]
+}
+
+type TriviaQuestion = {
+  category: string;
+  type: string;
+  difficulty: "easy" | "medium" | "hard";
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[]
+}
+
+type TriviaQuestionsResponse = {
+  response_code: number;
+  results: TriviaQuestion[]
+}
 
 @Injectable({
   providedIn: 'root',
@@ -10,30 +31,32 @@ import { Quiz, Question } from './quiz.model';
 export class QuestionsService {
   constructor(private http: HttpClient) {}
 
-  // public getQuizzes() {
-  //   return this.http.get(`./assets/quiz-list.json`).pipe(
-  //     map((result: any[]) => {
-  //       return result.map(
-  //         (r) => new Quiz(r.label, r.name, r.description, r.fileName)
-  //       );
-  //     })
-  //   );
-  // }
-
   public getQuizzes() {
-    return this.http.get(`./assets/quiz-list.json`).pipe(
-      map((result: any[]) => {
-        return result.map(
-          (r) => new Quiz(r.label, r.name, r.description, r.fileName)
+    return this.http.get(`https://opentdb.com/api_category.php`).pipe(
+      map((result: TriviaCategories) => {
+        return result.trivia_categories.map(
+          (r) => new Quiz('' + r.id, r.name)
         );
       })
     );
   }
 
-  public getQuestions(fileName: string) {
-    return this.http.get(`./assets/${fileName}.json`).pipe(
-      map((result: any[]) => {
-        return result.map((r, index, array) => new Question(r.label, r.choices, index, array.length));
+  convertTriviaQuestionToQuestion(tq: TriviaQuestion, index: number, length: number): Question {
+    let choices: Choice[] = tq.incorrect_answers.map((answer) => {
+      return new Choice(answer, false);
+    });
+    choices.push(new Choice(tq.correct_answer, true));
+    return new Question(tq.question, choices, index, length);
+  }
+
+  public getQuestions(categoryId: string): Observable<Question[]> {
+    return this.http.get(`https://opentdb.com/api.php?amount=10&category=${categoryId}&type=multiple`).pipe(
+      map((data: TriviaQuestionsResponse) => {
+        console.log(data);
+        let abc = data.results.map((tq: TriviaQuestion, index, array) => {
+          return this.convertTriviaQuestionToQuestion(tq, index, array.length);
+        })
+        return abc;
       })
     );
   }
